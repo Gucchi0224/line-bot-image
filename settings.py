e7d0caf57132c7d274307494f59ae0c838f92c14
Gucchi0224 <103201141+Gucchi0224@users.noreply.github.com>
2022-07-25 17:40:31 +0900

    fix settings.py

--- settings.py
+++ settings.py
@@ -11,3 +11,11 @@ YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % BUCKET_NAME
+MEDIA_URL = S3_URL
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
