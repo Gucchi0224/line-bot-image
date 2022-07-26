a72670cbf19d5d09a7b830e5e0326719462ba475
otabelab <otabelab.kyutech0406@gmail.com>
2022-07-26 16:31:40 +0900

    fix main.py

--- main.py
+++ main.py
@@ -110,8 +110,8 @@ def handle_message(event):
     for img_url, _ in rank[:5]:
         cloth_info = df[df["画像URL"]==img_url]
         flex_json_data["hero"]["url"] = img_url
-        print("{} -> {}".format(cloth_info["商品名"], type(cloth_info["商品名"])))
-        flex_json_data["body"]["contents"][0]["text"] = cloth_info["商品名"]
+        print(cloth_info["商品名"].values)
+        flex_json_data["body"]["contents"][0]["text"] = cloth_info["商品名"].values
         #flex_json_data["body"]["contents"][1]["contents"][0]["text"] = cloth_info["価格"]
         #flex_json_data["footer"]["contents"][0]["action"]["uri"] = cloth_info["URL"]
         d_flex["contents"].append(flex_json_data)
