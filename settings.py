8f31d87eafe44150cba88fbc053840fdc3b0141d
Gucchi0224 <103201141+Gucchi0224@users.noreply.github.com>
2022-07-26 21:17:02 +0900

    fix settings.py

--- settings.py
+++ settings.py
@@ -6,16 +6,8 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-# LINE
 YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-S3_URL = 'http://%s.s3.amazonaws.com/' % BUCKET_NAME
-MEDIA_URL = S3_URL
-
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
